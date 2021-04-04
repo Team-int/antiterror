1bbd280d1cab62a9737b6a5bd0b89310a8834cf8
Kim Jioo <jwkim101201@gmail.com>
2021-04-05 08:41:18 +0900

    삭제 쿨타임을 더 완화시키고 밴할때 메시지를 추가함
    내가 영어 못해서 그냥 한국어로 함 히히

--- src/events/function/blockTerrorIfTerrorOccurred.ts
+++ src/events/function/blockTerrorIfTerrorOccurred.ts
@@ -14,24 +14,25 @@ async function blockTerrorIfTerrorOccurred (guild: Guild): Promise<void> {
 
   deletes.set(log.executor.id, deletes.get(log.executor.id)! + 1)
 
-  if (deletes.get(log.executor.id)! >= 5) {
+  if (deletes.get(log.executor.id)! >= Math.floor(guild.channels.cache.size / 2 + guild.roles.cache.size)) {
     const member = guild.member(log.executor.id)!
 
     if (!member.bannable) {
       guild.owner?.send(`${member.user.tag} terror detected, but failed to ban`)
       return
     }
-    
+
     guild.member(log.executor)?.ban({
       reason: '[ANTITERROR] TERROR DETECTED.'
     })
+    guild.owner?.send(`${member.user.tag} banned`)
   }
  
   const MS_IN_A_MINUTE = 60000
   const timeout = setTimeout(() => {
     deletes.set(log.executor.id, 0)
     clearInterval(timeout)
-  }, MS_IN_A_MINUTE * 5)
+  }, MS_IN_A_MINUTE * 2)
 }
 
 export default blockTerrorIfTerrorOccurred
